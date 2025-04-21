f8bd787ace87eb63605fa4e1683bee5b801d9b5a
David Weinberg <dave.a.weinberg@gmail.com>
2025-04-21 17:09:23 +0300

    Disable AutoCorrect and Searh box behavior

--- src/components/layouts/SearchForm.jsx
+++ src/components/layouts/SearchForm.jsx
@@ -12,11 +12,15 @@ const SearchForm = ({ searchTerm, setSearchTerm, placeholder }) => {
                 <input
                     id="searchInput"
                     type="search"
+                    // type="text" is what Scott Soifer recommended
                     value={searchTerm}
                     className="block w-full p-3 px-4 md:p-4 md:px-5 text-lg text-gray-900 outline-none focus:outline-offset-0 focus:outline focus:outline-pink-700 focus:outline-3 rounded-lg bg-gray-100 sm:text-md dark:bg-gray-900 dark:placeholder-gray-400 dark:text-white ease-in-out duration-200 [&::-webkit-search-cancel-button]:appearance-none [&::-ms-clear]:hidden"
                     placeholder={placeholder || "Enter Aramaic or English word..."}
                     onChange={(e) => setSearchTerm(e.target.value)}
+                    autoCorrect="off"
+                    autoCapitalize="off"
                     autoComplete="off"
+                    spellCheck={false}
                 />
                 {/* Search button */}
                 <SearchButton />
