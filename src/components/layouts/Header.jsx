3b43bbe25db47ca58ca9f6655c3ac0f22b2014f6
David Weinberg <dave.a.weinberg@gmail.com>
2025-04-27 06:32:24 +0300

    fixed padding for header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -5,13 +5,15 @@ import GrammarButton from "../buttons/GrammarButton";
 
 const Header = () => {
     return (
-        <header className="mx-2">
-            <div className="flex justify-between">
+        // <header className="mx-2 mt-4">
+<header style={{ padding: '0px' }}> {/* Override with inline styles */}
+<div className="flex justify-between w-full p-3 px-4 md:p-4 md:px-5 rounded-lg bg-gray-100 dark:bg-gray-900">
                 <Logo />
+                <GrammarButton />
                 <div className="flex flex-col space-y-2">
                     <ThemeButton />
                     <TextSizeButton />
-                    <GrammarButton />
+                    
                 </div>
             </div>
         </header>
